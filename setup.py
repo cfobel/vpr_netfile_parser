0ffb89f5e89a28d57e731908b180f2ac01d376d6
Christian Fobel <christian@fobel.net>
2015-04-05 12:06:17 -0400

    Add version.py to extract version from git tags

--- setup.py
+++ setup.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python
 import os
 import sys
+sys.path.insert(0, '.')
 from distutils.core import setup, Extension
 from Cython.Build import cythonize
 
 import vpr_netfile_parser
+import version
 
 include_dirs = vpr_netfile_parser.get_includes()
 sys.path += include_dirs
@@ -20,13 +22,13 @@ cython_exts = [Extension('vpr_netfile_parser.VprNetParser',
 
 
 setup(name = "vpr_netfile_parser",
-    version = "0.0.1",
-    description = "C++ VPR `.net` netlist-file parser, with Cython bindings",
-    keywords = "fpga placement python C++ cython vpr",
-    author = "Christian Fobel",
-    url = "https://github.com/cfobel/vpr_netfile_parser",
-    license = "GPL",
-    long_description = """""",
-    packages = ['vpr_netfile_parser'],
-    package_data={'vpr_netfile_parser': ['VprNetParser.hpp']},
-    ext_modules=cythonize(cython_exts))
+      version=version.getVersion(),
+      description = "C++ VPR `.net` netlist-file parser, with Cython bindings",
+      keywords = "fpga placement python C++ cython vpr",
+      author = "Christian Fobel",
+      url = "https://github.com/cfobel/vpr_netfile_parser",
+      license = "GPL",
+      long_description = """""",
+      packages = ['vpr_netfile_parser'],
+      package_data={'vpr_netfile_parser': ['VprNetParser.hpp']},
+      ext_modules=cythonize(cython_exts))
